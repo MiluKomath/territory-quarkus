cd9e75dbec52272a797ab37e931a7f3dd244ad6e
MiluKomath <milu@strebentechnik.com>
2022-05-20 10:27:53 +0530

    Made some update in TerritoryResource class.

--- src/main/java/com/territory/TerritoryResourse.java
+++ src/main/java/com/territory/TerritoryResourse.java
@@ -4,9 +4,13 @@ import java.net.URI;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -17,15 +21,45 @@ public class TerritoryResourse {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@Transactional
-	public Response createEmployee(TerritoryEntity territoryEntity) {
+	public Response createTerritory(TerritoryEntity territoryEntity) {
 		territoryEntity.persist();
 		return Response.created(URI.create("/territory/" + territoryEntity.id)).build();
 	}
-	
-	 @GET
-	    @Produces(MediaType.APPLICATION_JSON)
-	    public Response listAllTerritories() {
-	        return Response.ok(TerritoryEntity.listAll()).build();
-	    }
-    
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response listAllTerritories() {
+		return Response.ok(TerritoryEntity.listAll()).build();
+	}
+
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public TerritoryEntity getTerritoryById(@PathParam("id") Long id) {
+		return TerritoryEntity.findById(id);
+	}
+
+	@DELETE
+	@Path("/{id}")
+	@Transactional
+	public void deleteTerritory(@PathParam("id") Long id) {
+		TerritoryEntity.deleteById(id);
+
+	}
+
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Transactional
+	public TerritoryEntity putTerritory(@PathParam("id") Long id, TerritoryEntity territoryEntity) {
+
+		TerritoryEntity territoryEntity1 = TerritoryEntity.findById(id);
+		if (territoryEntity1 == null) {
+			throw new NotFoundException();
+		}
+		territoryEntity1.name = territoryEntity.name;
+		return territoryEntity1;
+	}
+
 }
